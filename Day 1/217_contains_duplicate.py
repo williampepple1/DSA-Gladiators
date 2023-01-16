326ab357b22bfa1f982cea6cea043276781d66f9
kolaoba <kolaobajuluwa@gmail.com>
2023-01-16 22:17:21 +0100

    updated 217_contains_duplicate.py with solutions

--- Day 1/217_contains_duplicate.py
+++ Day 1/217_contains_duplicate.py
@@ -3,3 +3,16 @@
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+		checker = {}
+		for num in nums:
+			if checker[num] is True:
+				return True
+			else:
+				checker[num] = True
+		return False
+
+
+		"""
+		Alternate Solution
+		return True if len(nums) != len(set(nums)) else False
+		"""
