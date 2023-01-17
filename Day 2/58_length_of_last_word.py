0c68522200c4066c66456864349ea50def7ed079
Wolemercy <wolemercy@gmail.com>
2023-01-17 14:41:57 +0100

    Add Day-2 solutions

--- Day 2/58_length_of_last_word.py
+++ Day 2/58_length_of_last_word.py
@@ -4,3 +4,15 @@
 
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        ## Time - O(n)
+        ## Space - O(1)
+
+        lastLength = 0
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == ' ':
+                if lastLength > 0:
+                    return lastLength
+            else:
+                lastLength += 1
+        
+        return lastLength
