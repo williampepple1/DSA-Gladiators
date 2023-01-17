246a98758c1e7dbbe5553495f402888f2103598b
William Pepple <williampepple1@gmail.com>
2023-01-17 03:40:39 +0100

    Day 2 Questions uploaded

---
+++ Day 2/1244_design_a_leaderboaard.py
@@ -0,0 +1,27 @@
+# Design a Leaderboard class, which has 3 functions:
+
+# addScore(playerId, score): Update the leaderboard by adding score to the given player's score. If there is no player with such id in the leaderboard, add him to the leaderboard with the given score.
+# top(K): Return the score sum of the top K players.
+# reset(playerId): Reset the score of the player with the given id to 0 (in other words erase it from the leaderboard). It is guaranteed that the player was added to the leaderboard before calling this function.
+# Initially, the leaderboard is empty.
+
+class Leaderboard:
+
+    def __init__(self):
+        
+
+    def addScore(self, playerId: int, score: int) -> None:
+        
+
+    def top(self, K: int) -> int:
+        
+
+    def reset(self, playerId: int) -> None:
+        
+
+
+# Your Leaderboard object will be instantiated and called as such:
+# obj = Leaderboard()
+# obj.addScore(playerId,score)
+# param_2 = obj.top(K)
+# obj.reset(playerId)
