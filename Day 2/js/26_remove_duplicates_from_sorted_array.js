ae5a56218f4c1b1fdc8ab2fe18b67920c67bcfec
Osemeke Echenim <echenim.osemeke@gmail.com>
2023-01-17 10:15:15 +0100

    day 2 solutions

---
+++ Day 2/js/26_remove_duplicates_from_sorted_array.js
@@ -0,0 +1,23 @@
+/**
+ * Linear 
+ * Time O(N) | Space O(1)
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = (nums) => {
+    let [left, right] = [0, 0];
+
+    while (right < nums.length) {
+        const [leftVal, rightVal] = [nums[left], nums[right]];
+
+        const isEqual = (rightVal === leftVal);
+        if (!isEqual) {
+            left++;
+            nums[left] = rightVal;
+        }
+
+        right++;
+    }
+
+    return (left + 1);
+};
