3305cc8d42f6409798369178556e7d6e696da787
William Pepple <williampepple1@gmail.com>
2023-01-17 14:29:13 +0100

    Day 2 Solutions

--- Day 2/1244_design_a_leaderboaard.py
+++ Day 2/1244_design_a_leaderboaard.py
@@ -1,23 +1,20 @@
-# Design a Leaderboard class, which has 3 functions:
-
-# addScore(playerId, score): Update the leaderboard by adding score to the given player's score. If there is no player with such id in the leaderboard, add him to the leaderboard with the given score.
-# top(K): Return the score sum of the top K players.
-# reset(playerId): Reset the score of the player with the given id to 0 (in other words erase it from the leaderboard). It is guaranteed that the player was added to the leaderboard before calling this function.
-# Initially, the leaderboard is empty.
-
 class Leaderboard:
 
     def __init__(self):
-        
+        self.scores = {}
 
     def addScore(self, playerId: int, score: int) -> None:
-        
+        if playerId in self.scores:
+            self.scores[playerId] += score
+        else:
+            self.scores[playerId] = score
+         
 
     def top(self, K: int) -> int:
-        
+        return sum(heapq.nlargest(K, self.scores.values()))
 
     def reset(self, playerId: int) -> None:
-        
+        self.scores[playerId] = 0
 
 
 # Your Leaderboard object will be instantiated and called as such:
--- Day 2/58_length_of_last_word.py
+++ Day 2/58_length_of_last_word.py
@@ -1,6 +1,15 @@
 # Given a string s consisting of words and spaces, return the length of the last word in the string.
 
 # A word is a maximal substring consisting of non-space characters only.
-
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        i = len(s) - 1
+        length = 0
+
+        while s[i] == " ":
+            i -= 1
+        while i >= 0 and s[i] != " ":
+            length += 1
+            i -= 1
+        return length
+
