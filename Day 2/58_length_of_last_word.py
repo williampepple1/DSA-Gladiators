5cb1ed34bb4b136468682d2665decda10ed5207f
kolaoba <kolaobajuluwa@gmail.com>
2023-01-17 23:30:39 +0100

    Added solutions for 58_length_of_last_word

--- Day 2/58_length_of_last_word.py
+++ Day 2/58_length_of_last_word.py
@@ -4,3 +4,14 @@
 
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        return len(s.split(' ')[-1])
+
+        """
+        Alternate Solution
+        s = s.strip()
+        length = len(s) - 1
+        for i in range(length, -1, -1):
+            if s[i] == ' ':
+                return length - i
+        return len(s)
+        """
