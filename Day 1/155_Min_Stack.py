1ca201c1eb9a14cf4fd0cccd9be7ece98e8f027e
oluwole <oluwoleajayijnr@gmail.com>
2023-01-18 10:03:14 +0100

    min stack

--- Day 1/155_Min_Stack.py
+++ Day 1/155_Min_Stack.py
@@ -13,18 +13,22 @@
 class MinStack:
 
     def __init__(self):
-        
+        self.stack = []
 
     def push(self, val: int) -> None:
+        if self.stack:
+            self.stack.append((val, min(self.stack[-1][1], val)))
+        else:
+            self.stack.append((val, val))
         
-
     def pop(self) -> None:
-        
+        self.stack.pop()        
 
     def top(self) -> int:
-        
+        return self.stack[-1][0]
 
     def getMin(self) -> int:
+        return self.stack[-1][1]
         
 
 
