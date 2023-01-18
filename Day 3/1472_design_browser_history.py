bfc35301328f81da86f35b6271fe71944cc858d0
William Pepple <williampepple1@gmail.com>
2023-01-18 09:27:21 +0100

    Day 3 Questions Added

---
+++ Day 3/1472_design_browser_history.py
@@ -0,0 +1,30 @@
+# You have a browser of one tab where you start on the homepage and you can visit another url, get back in the history number of steps or move forward in the history number of steps.
+
+# Implement the BrowserHistory class:
+
+# BrowserHistory(string homepage) Initializes the object with the homepage of the browser.
+# void visit(string url) Visits url from the current page. It clears up all the forward history.
+# string back(int steps) Move steps back in history. If you can only return x steps in the history and steps > x, you will return only x steps. Return the current url after moving back in history at most steps.
+# string forward(int steps) Move steps forward in history. If you can only forward x steps in the history and steps > x, you will forward only x steps. Return the current url after forwarding in history at most steps.
+
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        
+
+    def visit(self, url: str) -> None:
+        
+
+    def back(self, steps: int) -> str:
+        
+
+    def forward(self, steps: int) -> str:
+        
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
