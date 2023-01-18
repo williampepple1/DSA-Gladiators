d066878d736ebc0b61b2438b9ea8c4696763de7c
William Pepple <williampepple1@gmail.com>
2023-01-18 09:27:21 +0100

    Day 3 Questions Added

---
+++ Day 3/705_design_hashset.py
@@ -0,0 +1,28 @@
+# Design a HashSet without using any built-in hash table libraries.
+
+# Implement MyHashSet class:
+
+# void add(key) Inserts the value key into the HashSet.
+# bool contains(key) Returns whether the value key exists in the HashSet or not.
+# void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+
+class MyHashSet:
+
+    def __init__(self):
+        
+
+    def add(self, key: int) -> None:
+        
+
+    def remove(self, key: int) -> None:
+        
+
+    def contains(self, key: int) -> bool:
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
