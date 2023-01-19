6edb5ac9db27fa4ef26610a47120424692322399
unknown <oluwoleajayijnr@gmail.com>
2023-01-19 18:36:37 +0100

    unique character

--- Day 4/387_first_unique_character_in_a_string.py
+++ Day 4/387_first_unique_character_in_a_string.py
@@ -1,4 +1,10 @@
 # Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -2
-
+import 
 class Solution:
     def firstUniqChar(self, s: str) -> int:
+        count = Counter(s)
+
+        for i, num in enumerate(s):
+            if count[num] == 1:
+                return i
+        return -1
