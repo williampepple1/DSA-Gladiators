5037f06eed4cacbdecb0e7fef3e4ee93425c1710
William Pepple <williampepple1@gmail.com>
2023-01-19 20:04:09 +0100

    Day 4 Solutions

--- Day 4/387_first_unique_character_in_a_string.py
+++ Day 4/387_first_unique_character_in_a_string.py
@@ -2,3 +2,16 @@
 
 class Solution:
     def firstUniqChar(self, s: str) -> int:
+        my_hashmap = {}
+
+        for i, val in enumerate(s):
+            if val in my_hashmap:
+                my_hashmap[val] += 1
+            else:
+                my_hashmap[val] = 1
+        
+        for i, val in enumerate(s):
+            if my_hashmap[val] == 1:
+                return i
+        return -1
+        
