885c9d8b9b26dc1d97f1d96df0b144229e943f64
Wolemercy <wolemercy@gmail.com>
2023-01-23 22:23:36 +0100

    Add Day 7 Solution

--- week_1/Day_7/704_binary_search.py
+++ week_1/Day_7/704_binary_search.py
@@ -1,6 +1,23 @@
-    # Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+# Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 
-    # You must write an algorithm with O(log n) runtime complexity.
+# You must write an algorithm with O(log n) runtime complexity.
+
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
