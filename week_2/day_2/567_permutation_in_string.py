d9a509c5e39448a4d7e815f571ef27af1c855829
William Pepple <williampepple1@gmail.com>
2023-01-24 22:12:17 +0100

    Today's Questions Uploaded

--- week_2/day_2/567_permutation_in_string.py
+++ week_2/day_2/567_permutation_in_string.py
@@ -22,10 +22,22 @@ Constraints:
 s1 and s2 consist of lowercase English letters.
 """
 
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        cntr, w, match = Counter(s1), len(s1), 0     
+
+        for i in range(len(s2)):
+            if s2[i] in cntr:
+                if not cntr[s2[i]]: match -= 1
+                cntr[s2[i]] -= 1
+                if not cntr[s2[i]]: match += 1
+
+            if i >= w and s2[i-w] in cntr:
+                if not cntr[s2[i-w]]: match -= 1
+                cntr[s2[i-w]] += 1
+                if not cntr[s2[i-w]]: match += 1
+
+            if match == len(cntr):
+                return True
+
+        return False
