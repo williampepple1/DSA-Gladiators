c7ae22c8e6536546273268012a90d4583e8d58c9
zainablawal2211 <zainablawal2211@gmail.com>
2023-01-25 14:07:49 -0800

    completed week 2 day 3 task

--- week_2/day_3/380_insert_delete_getrandom.py
+++ week_2/day_3/380_insert_delete_getrandom.py
@@ -1,26 +1,35 @@
 # Implement the RandomizedSet class:
+import random
 
-# RandomizedSet() Initializes the RandomizedSet object.
-# bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
-# bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
-# int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
-# You must implement the functions of the class such that each function works in average O(1) time complexity
 
+# RandomizedSet() Initializes the RandomizedSet object. bool insert(int val) Inserts an item val into the set if not
+# present. Returns true if the item was not present, false otherwise. bool remove(int val) Removes an item val from
+# the set if present. Returns true if the item was present, false otherwise. int getRandom() Returns a random element
+# from the current set of elements (it's guaranteed that at least one element exists when this method is called).
+# Each element must have the same probability of being returned. You must implement the functions of the class such
+# that each function works in average O(1) time complexity
 
 class RandomizedSet:
 
     def __init__(self):
-        
+        self.my_set = set()
 
     def insert(self, val: int) -> bool:
-        
+        if val in self.my_set:
+            return False
+        else:
+            self.my_set.add(val)
+            return True
 
     def remove(self, val: int) -> bool:
-        
+        if val in self.my_set:
+            self.my_set.remove(val)
+            return True
+        else:
+            return False
 
     def getRandom(self) -> int:
-        
-
+        return random.choice(list(self.my_set))
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
