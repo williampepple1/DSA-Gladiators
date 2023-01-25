a769f9edc2f6fea73778db611b3dc4da70811ca6
zainablawal2211 <zainablawal2211@gmail.com>
2023-01-25 12:18:19 -0800

    completed week 1 day 1 task

--- week_1/Day 1/217_contains_duplicate.py
+++ week_1/Day 1/217_contains_duplicate.py
@@ -1,5 +1,14 @@
 # Given an integer array nums, return true if any value appears at least twice in the array,
 #  and return false if every element is distinct.
+from typing import List
+
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
+        return False
