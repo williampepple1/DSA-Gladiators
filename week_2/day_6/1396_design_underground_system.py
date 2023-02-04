1267e8cf68b8133df5907724397d89998665d27c
William Pepple <williampepple1@gmail.com>
2023-02-04 10:10:44 +0100

    Update 1396_design_underground_system.py

--- week_2/day_6/1396_design_underground_system.py
+++ week_2/day_6/1396_design_underground_system.py
@@ -13,7 +13,7 @@
 # The time it takes to travel from startStation to endStation may be different from the time it takes to travel from endStation to startStation.
 # There will be at least one customer that has traveled from startStation to endStation before getAverageTime is called.
 # You may assume all calls to the checkIn and checkOut methods are consistent. If a customer checks in at time t1 then checks out at time t2, then t1 < t2. All events happen in chronological order.
-
+import defaultdict
 
 class UndergroundSystem:
 
@@ -22,15 +22,19 @@ class UndergroundSystem:
     #           27, Leyton, 10                   27, Waterloo, 20 (10)  
 
     def __init__(self):
-
+        self.inside = defaultdict(tuple)
+        self.outside = defaultdict(list)
         
     def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.inside[id] = (t, stationName)
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:    
-
+        starttime, startstation = self.inside[id]
+        total = t - starttime
+        self.outside[(startstation, stationName)].append(total)
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-  
+        return sum(self.outside[(startStation, endStation)])/len(self.outside[(startStation, endStation)])
 
 
         
