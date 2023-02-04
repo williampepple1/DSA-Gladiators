60db479fb821c6092745acebbfe3d49a869dbfaf
William Pepple <williampepple1@gmail.com>
2023-02-04 12:14:26 +0100

    Update 35_search_insert_position.py

--- week_3/day_4/35_search_insert_position.py
+++ week_3/day_4/35_search_insert_position.py
@@ -29,3 +29,18 @@ Link: https://leetcode.com/problems/search-insert-position/
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if target == nums[mid]:
+                return mid
+            else:
+                if nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return l
