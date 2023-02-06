5a29a3615955cad894afaf85192f24125db2d035
unknown <oluwoleajayijnr@gmail.com>
2023-02-06 17:52:19 +0100

    convert binary

--- week_3/day_5/1290_convert_binary_num_in_a_linkedlist_to_int.py
+++ week_3/day_5/1290_convert_binary_num_in_a_linkedlist_to_int.py
@@ -35,3 +35,9 @@ Link: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-in
 
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
+        num = head.val
+        while head.next:
+            num = num * 2 + head.next.val
+            head = head.next
+
+        return num
