996d1d134caa464c1a8ec97a80054deb5fe58de3
unknown <oluwoleajayijnr@gmail.com>
2023-02-06 19:57:40 +0100

    longest common prefix

--- week_3/day_3/14_longest_common_prefix.py
+++ week_3/day_3/14_longest_common_prefix.py
@@ -27,3 +27,12 @@ Link: https://leetcode.com/problems/longest-common-prefix/
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+                res += strs[0][i]
+
+        return res
