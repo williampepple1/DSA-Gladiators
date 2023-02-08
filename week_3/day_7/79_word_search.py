7cb16a01810983e042dd65482b93b32115d2a8fe
zainablawal2211 <zainablawal2211@gmail.com>
2023-02-08 22:31:13 +0100

    completed week 3 day 7 challenge.

--- week_3/day_7/79_word_search.py
+++ week_3/day_7/79_word_search.py
@@ -36,6 +36,28 @@ Follow up: Could you use search pruning to make your solution faster with a larg
 Link: https://leetcode.com/problems/word-search/
 
 """
+from typing import List
+
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or word[i] != board[r][c] or (r, c) in path:
+                return False
+            path.add((r, c))
+            res = (dfs(r + 1, c, i + 1) or (r - 1, c, i + 1) or (r, c + 1, i + 1) or (r, c - 1, i + 1))
+
+            path.remove((r, c))
+
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0): return True
+
+        return False
