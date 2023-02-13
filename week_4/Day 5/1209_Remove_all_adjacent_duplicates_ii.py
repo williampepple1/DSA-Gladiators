159e20064c346cef06710ebdbea2e4ae0a6f68d6
unknown <oluwoleajayijnr@gmail.com>
2023-02-13 18:03:23 +0100

    remove duplicates

--- week_4/Day 5/1209_Remove_all_adjacent_duplicates_ii.py
+++ week_4/Day 5/1209_Remove_all_adjacent_duplicates_ii.py
@@ -1,3 +1,12 @@
-function removeDuplicates(s: string, k: number): string {
+def removeDuplicates(s, k):
+    stack = []
+    for i in range(len(s)):
+        if stack and s[i] == stack[-1][0]:
+            if stack[-1][1] + 1 == k:
+                stack.pop()
+            else:
+                stack[-1][1] += 1
+        else:
+            stack.append((s[i], 1))
 
-};
+    return "".join(i * c for i, c in stack)
