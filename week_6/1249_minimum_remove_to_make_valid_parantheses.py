7fceec267bdbb161592d1a07dd3b11f5d308d106
zainablawal2211 <zainablawal2211@gmail.com>
2023-02-21 05:21:01 +0100

    completed week 6 day 1 challenge.

--- week_6/1249_minimum_remove_to_make_valid_parantheses.py
+++ week_6/1249_minimum_remove_to_make_valid_parantheses.py
@@ -1,2 +1,18 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
+        valid = list(s)
+        stack = []
+
+        for i in range(len(valid)):
+            if valid[i] == "(":
+                stack.append(i)
+            elif valid[i] == ")":
+                if stack:
+                    stack.pop()
+                else:
+                    valid[i] = ""
+
+        for j in stack:
+            valid[j] = ""
+
+        return "".join(valid)
