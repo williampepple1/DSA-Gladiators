d4b728b4decd6fe8a638602cadcf1fcc68944b51
zainablawal2211 <zainablawal2211@gmail.com>
2023-02-22 11:40:30 +0100

    completed 20_valid_parentheses.py challenge.

--- week_6/20_valid_parentheses.py
+++ week_6/20_valid_parentheses.py
@@ -1,2 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        stack = []
+
+        bracket = {
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        }
+
+        for i in range(len(s)):
+            if s[i] in bracket.values():
+                stack.append(s[i])
+                continue
+
+            if len(stack) != 0 and bracket[s[i]] == stack[-1]:
+                stack.pop()
+                continue
+
+            else:
+                return False
+
+        return len(stack) == 0
